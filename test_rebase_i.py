6c71e859a6cc4fec1eb2b95a77ef60c2b755d85a
Your Name <you@example.com>
2024-03-23 01:17:48 +0900

    test_rebase_i

---
+++ test_rebase_i.py
@@ -0,0 +1,90 @@
+
+
+import unittest
+import os
+import shutil
+import sys
+import re
+import hashlib
+import ptyexec
+import io
+from datetime import datetime
+from testutil import *
+
+class GitTest(unittest.TestCase):
+
+    def setUp(self):
+        os.chdir(DIR_ORG)
+        if os.path.exists("testgitdir"):
+            shutil.rmtree("testgitdir")
+        os.mkdir("testgitdir")
+        os.chdir("testgitdir")
+        cmd_stdout("git init")
+        cmd_stdout("git config user.name HirofumiShinke")
+        cmd_stdout("git config user.email hiro.shinke@gmail.com")
+
+
+    def test_rebase_txt1(self):
+
+        make_file("hello.txt","""\
+hello, world
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m A')
+
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m B')
+
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+goodby america
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m C')
+
+
+        make_file("hello.txt","""\
+hello, world
+goodby japan
+goodby europe
+goodby america
+"""
+                  )
+        cmd_stdout("git add hello.txt")
+        cmd_stdout('git commit -m D')
+
+        ret = cmd_stdout("git log --graph --abbrev-commit --oneline")
+        self.assertEqual("""\
+* 967e46b D
+* 40c9072 C
+* b3db10b B
+* 70809e4 A
+"""
+                         ,ret)
+        
+        ptyexec.pty_exec('git rebase -i master~3',
+                         keyseq=b"/pick\rncwsquash\x1b:wq\r:wq\r",
+                         delay=0.2)
+
+        ret = cmd_stdout("git log --graph --abbrev-commit --oneline")
+        self.assertEqual("""\
+* 5fec20a C
+* b3db10b B
+* 70809e4 A
+"""
+                         ,ret)
+                         
+        
+if __name__ == "__main__":
+    unittest.main()
+
+
+    
